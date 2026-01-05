a1812e2d84b3ffaabb4e8864910626e31eb0109f
Ansh gajbhiye <krishnagajbhiye1@gmail.com>
2026-01-06 00:10:55 +0530

    added route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Error from "./components/Error";
+import { Route, Switch } from "react-router-dom";
 
-const App = () => {
+function App() {
   return (
-    <div>fald</div>
-  )
+    <div className="App">
+      <Switch>
+        <Route path="/" component={Login} exact />
+        <Route path="/signup" component={Signup} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
