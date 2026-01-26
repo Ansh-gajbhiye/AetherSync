832106b9b4cdc8e05ca04a6b6a393625311d993e
Ansh gajbhiye <krishnagajbhiye1@gmail.com>
2026-01-26 21:05:57 +0530

    Implement system RAM check with startup warning

--- server/music-library/MusicManager.js
+++ server/music-library/MusicManager.js
@@ -4,3 +4,14 @@ import path from 'path';
 
 const homedir= os.homedir();
 console.log("Home Directory:", homedir);
+
+const startathersync = () => {
+ console.log("Starting AtherSync Service...");
+ const freeMemGB = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
+ console.log(` Memory: ${freeMemGB} GB`);
+ if((freeMemGB) < 2 ){
+    console.log("Insufficient memory ...");
+    
+ }
+}
+startathersync();
