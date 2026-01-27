7e5535bfdf62f0e68add3d065077886ffa0a095e
Ansh gajbhiye <krishnagajbhiye1@gmail.com>
2026-01-27 22:36:30 +0530

    add fs module to check if folder exists

--- server/music-library/MusicManager.js
+++ server/music-library/MusicManager.js
@@ -1,21 +1,29 @@
 import os from 'os';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 console.log("Script file:", __filename);
-// const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
+const homedir= os.homedir()
+console.log(homedir);
 
-// const libraryPath= path.join(__dirname,"OneDrive", "Desktop", "music-library" );
-// const filePath= path.join(__dirname, "server-logs.txt" );
+const libraryPath= path.join(homedir,"OneDrive", "Desktop", "music-library" );
+const filePath= path.join(libraryPath, "server-logs.txt" );
 
-// const startathersync = () => {
-//  console.log("Starting AtherSync Service...");
-//  const freeMemGB = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
-//  console.log(` Memory: ${freeMemGB} GB`);
-//  if((freeMemGB) < 2 ){
-//     console.log("Insufficient memory ...");
-//  }
-// }
-// startathersync();
-// console.log("Script directory:", __dirname);
+const startathersync = () => {
+ console.log("Starting AtherSync Service...");
+ const freeMemGB = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
+ console.log(` Memory: ${freeMemGB} GB`);
+ if((freeMemGB) < 2 ){
+    console.log("Insufficient memory ...");
+ }
+}
+if (!fs.existsSync(libraryPath)){
+   fs.mkdirSync(libraryPath);
+}
+fs.writeFileSync(filePath, "This is a test file for AtherSync audit log.");
+startathersync();
+console.log("Script directory:", __dirname);
+console.log("Folder & file created successfully!");
